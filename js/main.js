b57e8c6a81d00953bd32b5d887c824e1f21e343a
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-05-09 18:24:45 +0530

    design table

---
+++ js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+  $('.instructional-slider-wrapper').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    speed: 300,
+    variableWidth: true
+  });
+      
+  
+});
